7a566a78d1ca36a18ceac022d643e9f3bbf4c9c3
yukimelty <yuki.it.engineer@gmail.com>
2025-01-02 05:55:05 +0900

    use front_domain config to cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://127.0.0.1:4000"
+    origins Settings.front_domain
 
     resource "*",
       headers: :any,

0d07c4dcddc16099124d62ff85906c1667f487ca
yukimelty <yuki.it.engineer@gmail.com>
2025-01-03 20:43:20 +0900

    install factory_bot_rails and faker

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -43,6 +43,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  # FactoryBotの宣言を省略
+  config.include FactoryBot::Syntax::Methods
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
